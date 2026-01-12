82126e343c8f499cecfc2f6bde0b0528a5a98408
bagi <hassanachlou@gmail.com>
2026-01-12 20:00:11 +0100

    add validation to invite Request

--- src/main/java/com/ensa/agile/application/common/request/InviteRequest.java
+++ src/main/java/com/ensa/agile/application/common/request/InviteRequest.java
@@ -1,21 +1,36 @@
 package com.ensa.agile.application.common.request;
 
-import lombok.AllArgsConstructor;
+import com.ensa.agile.domain.global.exception.ValidationException;
+import com.ensa.agile.domain.global.utils.ValidationUtil;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Data
 public class InviteRequest {
-     private String email;
     private String productId;
+    private String email;
 
     public InviteRequest(String productId, InviteRequest req) {
         this.productId = productId;
         this.email = req.getEmail();
+    }
+
+    public InviteRequest(String productId, String email) {
+        if (email == null || email.isBlank()) {
+            throw new ValidationException("Email cannot be null or empty");
+        }
+        if (!ValidationUtil.isValidEmail(email)) {
+            throw new ValidationException("Email format is invalid");
+        }
+
+        if (productId == null || productId.isBlank()) {
+            throw new ValidationException("Product ID cannot be null or empty");
+        }
+        this.productId = productId;
+        this.email = email;
     }
 
     public InviteRequest(String email) {
