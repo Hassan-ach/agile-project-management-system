4ec9b1035f6802bbe910338eeb573b163a646d13
bagi <hassanachlou@gmail.com>
2026-01-14 02:43:21 +0100

    implemet user story controller

--- src/main/java/com/ensa/agile/presentation/controller/UserStoryController.java
+++ src/main/java/com/ensa/agile/presentation/controller/UserStoryController.java
@@ -1,15 +1,25 @@
 package com.ensa.agile.presentation.controller;
 
+import com.ensa.agile.application.common.response.DeleteResponse;
 import com.ensa.agile.application.story.request.UserStoryCreateRequest;
+import com.ensa.agile.application.story.request.UserStoryGetRequest;
+import com.ensa.agile.application.story.request.UserStoryUpdateRequest;
 import com.ensa.agile.application.story.response.UserStoryResponse;
 import com.ensa.agile.application.story.usecase.CreateUserStoryUseCase;
+import com.ensa.agile.application.story.usecase.DeleteUserStoryUseCase;
+import com.ensa.agile.application.story.usecase.GetUserStoryUseCase;
+import com.ensa.agile.application.story.usecase.UpdateUserStoryUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -18,6 +28,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserStoryController {
 
     private final CreateUserStoryUseCase createUserStoryUseCase;
+    private final GetUserStoryUseCase getUserStoryUseCase;
+    private final UpdateUserStoryUseCase updateUserStoryUseCase;
+    private final DeleteUserStoryUseCase deleteUserStoryUseCase;
 
     @PostMapping
     public ResponseEntity<UserStoryResponse>
@@ -25,7 +38,34 @@ public class UserStoryController {
                     @PathVariable String productId) {
 
         return ResponseEntity.status(HttpStatus.CREATED)
-            .body(createUserStoryUseCase.execute(
+            .body(createUserStoryUseCase.executeTransactionally(
                 new UserStoryCreateRequest(productId, request)));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserStoryResponse> getUserStoryById(
+        @PathVariable String productId, @PathVariable String id,
+        @RequestParam(name = "with", required = false) String with) {
+
+        return ResponseEntity.ok().body(
+            getUserStoryUseCase.executeTransactionally(
+                new UserStoryGetRequest(id, with)));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<UserStoryResponse>
+    updateUserStory(@PathVariable String productId, @PathVariable String id,
+                    @RequestBody UserStoryUpdateRequest request) {
+
+        return ResponseEntity.ok().body(
+            updateUserStoryUseCase.executeTransactionally(
+                new UserStoryUpdateRequest(productId, id, request)));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<DeleteResponse>
+    deleteUserStory(@PathVariable String productId, @PathVariable String id) {
+        deleteUserStoryUseCase.executeTransactionally(id);
+        return ResponseEntity.noContent().build();
     }
 }
