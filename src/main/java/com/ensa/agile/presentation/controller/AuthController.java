3d588eca0a3c601aabfd7d5298804a05c8b7bfc6
bagi <hassanachlou@gmail.com>
2026-01-18 00:26:34 +0100

    add /me to auth controller

--- src/main/java/com/ensa/agile/presentation/controller/AuthController.java
+++ src/main/java/com/ensa/agile/presentation/controller/AuthController.java
@@ -1,19 +1,24 @@
 package com.ensa.agile.presentation.controller;
 
-import com.ensa.agile.application.user.request.AuthenticationRequest;
-import com.ensa.agile.application.user.request.RefreshTokenRequest;
-import com.ensa.agile.application.user.request.RegisterRequest;
-import com.ensa.agile.application.user.response.AuthenticationResponse;
-import com.ensa.agile.application.user.usecase.LoginUseCase;
-import com.ensa.agile.application.user.usecase.RefreshTokenUseCase;
-import com.ensa.agile.application.user.usecase.RegisterUseCase;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ensa.agile.application.user.request.AuthenticationRequest;
+import com.ensa.agile.application.user.request.RefreshTokenRequest;
+import com.ensa.agile.application.user.request.RegisterRequest;
+import com.ensa.agile.application.user.response.AuthenticationResponse;
+import com.ensa.agile.application.user.response.UserInfoResponse;
+import com.ensa.agile.application.user.usecase.GetUserInfoUseCase;
+import com.ensa.agile.application.user.usecase.LoginUseCase;
+import com.ensa.agile.application.user.usecase.RefreshTokenUseCase;
+import com.ensa.agile.application.user.usecase.RegisterUseCase;
+
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,6 +28,7 @@ public class AuthController {
     private final RegisterUseCase registerUseCase;
     private final LoginUseCase loginUseCase;
     private final RefreshTokenUseCase refreshTokenUseCase;
+    private final GetUserInfoUseCase getUserInfoUseCase;
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
@@ -50,4 +56,11 @@ public class AuthController {
                 new RefreshTokenRequest(request));
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserInfoResponse> me() {
+        UserInfoResponse response =
+            getUserInfoUseCase.executeTransactionally(null);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+            }
 }
