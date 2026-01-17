4724fdfe21aeae5fec11779fc605a0184e4942bf
bagi <hassanachlou@gmail.com>
2026-01-17 22:10:44 +0100

    add constructor with throwablw to DataBaseTransactionException

--- src/main/java/com/ensa/agile/domain/global/exception/DataBaseTransactionException.java
+++ src/main/java/com/ensa/agile/domain/global/exception/DataBaseTransactionException.java
@@ -2,5 +2,6 @@ package com.ensa.agile.domain.global.exception;
 
 public class DataBaseTransactionException extends RuntimeException {
   public DataBaseTransactionException(String message) { super(message); }
+  public DataBaseTransactionException(String message, Throwable e) { super(message, e); }
   public DataBaseTransactionException() { super("Database error occurred"); }
 }
