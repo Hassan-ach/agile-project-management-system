8a61320e8f23999bf141ffe030389c298232b70f
bagi <hassanachlou@gmail.com>
2026-01-23 22:49:37 +0100

    change how transactionWrapper hanlde exceptions

--- src/main/java/com/ensa/agile/infrastructure/persistence/jpa/global/transaction/TransactionWrapper.java
+++ src/main/java/com/ensa/agile/infrastructure/persistence/jpa/global/transaction/TransactionWrapper.java
@@ -1,14 +1,13 @@
 package com.ensa.agile.infrastructure.persistence.jpa.global.transaction;
 
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.support.TransactionTemplate;
-
 import com.ensa.agile.application.global.transaction.ITransactionCallBack;
 import com.ensa.agile.application.global.transaction.ITransactionalWrapper;
 import com.ensa.agile.domain.global.exception.DataBaseTransactionException;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @RequiredArgsConstructor
 @Component
@@ -20,12 +19,17 @@ public class TransactionWrapper implements ITransactionalWrapper {
         return transactionTemplate.execute(status -> {
             try {
                 return callBack.execution();
-            }catch (DataIntegrityViolationException e) {
+            } catch (DataIntegrityViolationException e) {
                 status.setRollbackOnly();
-                throw new DataBaseTransactionException("Data integrity violation occurred during transaction.", e);
+                throw new DataBaseTransactionException(
+                    "Data integrity violation occurred during transaction.", e);
+            } catch (DataAccessException e) {
+                status.setRollbackOnly();
+                throw new DataBaseTransactionException(
+                    "An unexpected error occurred during transaction.", e);
             } catch (Exception e) {
                 status.setRollbackOnly();
-                throw new DataBaseTransactionException("An unexpected error occurred during transaction.", e);
+                throw e;
             }
         });
     }
