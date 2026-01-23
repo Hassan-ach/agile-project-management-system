e23e7db71a791d66f5b07a9ee4706b3a65d2c164
bagi <hassanachlou@gmail.com>
2026-01-23 22:43:24 +0100

    change the InvalidCredentialsException parent from ValidationException => RuntimeException

--- src/main/java/com/ensa/agile/application/user/exception/InvalidCredentialsException.java
+++ src/main/java/com/ensa/agile/application/user/exception/InvalidCredentialsException.java
@@ -1,8 +1,6 @@
 package com.ensa.agile.application.user.exception;
 
-import com.ensa.agile.domain.global.exception.ValidationException;
-
-public class InvalidCredentialsException extends ValidationException {
+public class InvalidCredentialsException extends RuntimeException {
     public InvalidCredentialsException(String message) { super(message); }
 
     public InvalidCredentialsException() { super("Invalid credentials "); }
