d0bcd6a7ccdb465760af01e79db648391745a320
bagi <hassanachlou@gmail.com>
2026-01-23 22:54:39 +0100

    add IPassword incoder impl based and @Profile (i add impl for test profile so that i can baypass pasasword hash validation)

--- src/main/java/com/ensa/agile/infrastructure/security/config/DevSecurityBeans.java
+++ src/main/java/com/ensa/agile/infrastructure/security/config/DevSecurityBeans.java
@@ -1,28 +1,26 @@
 package com.ensa.agile.infrastructure.security.config;
 
+import com.ensa.agile.application.user.security.IPasswordEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import com.ensa.agile.application.user.security.IPasswordEncoder;
-
 @Configuration
-@Profile("dev")
+@Profile("test")
 public class DevSecurityBeans {
 
     @Bean
-    public IPasswordEncoder passwordEncoderImpl(){
+    public IPasswordEncoder passwordEncoderImpl() {
         return new IPasswordEncoder() {
             @Override
             public String encode(final CharSequence pass) {
-            return pass.toString();
+                return pass.toString();
             }
             @Override
-            public boolean matches(final CharSequence rawPass, final String encodedPass) {
-            return rawPass.toString().equals(encodedPass);
+            public boolean matches(final CharSequence rawPass,
+                                   final String encodedPass) {
+                return rawPass.toString().equals(encodedPass);
             }
-            
         };
     }
-
 }
--- src/main/java/com/ensa/agile/infrastructure/security/config/SecurityBeans.java
+++ src/main/java/com/ensa/agile/infrastructure/security/config/SecurityBeans.java
@@ -1,20 +1,18 @@
 package com.ensa.agile.infrastructure.security.config;
 
+import com.ensa.agile.application.user.security.IPasswordEncoder;
+import com.ensa.agile.infrastructure.security.utils.PasswordEncoderImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import com.ensa.agile.application.user.security.IPasswordEncoder;
-import com.ensa.agile.infrastructure.security.utils.PasswordEncoderImpl;
-
 @Configuration
-@Profile("!dev")
+@Profile("!test")
 public class SecurityBeans {
 
     @Bean
-    public IPasswordEncoder passwordEncoderImpl(){
+    public IPasswordEncoder passwordEncoderImpl() {
         return new PasswordEncoderImpl(new BCryptPasswordEncoder());
     }
-
 }
